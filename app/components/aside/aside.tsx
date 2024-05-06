c55e1d6e864f79ed730dd5535ed1773de24f8fb4
ptchom <ptchom@eb.by>
2024-05-06 20:54:00 +0200

    feat: Die Seite wird entwickelt

--- app/components/aside/aside.tsx
+++ app/components/aside/aside.tsx
@@ -1,10 +1,12 @@
 import { ReactElement } from "react";
 import { Navigation } from "./navigation";
+import { ImAufbau } from "~/components/aside/im-aufbau";
 
 export const Aside = (): ReactElement => {
   return (
     <>
       <Navigation />
+      <ImAufbau />
     </>
   );
 };
---
+++ app/components/aside/im-aufbau.tsx
@@ -0,0 +1,4 @@
+export const ImAufbau = () => {
+  const styles = "text-red-500 text-xl font-semibold";
+  return <p className={styles}>Die Seite wird entwickelt</p>;
+};
