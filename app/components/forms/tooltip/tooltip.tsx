ff49879111fd8b7fac8fc24dc0fe27a36f45cb6f
ptchom <ptchom@eb.by>
2024-05-06 20:47:11 +0200

    feat: form better

--- app/components/forms/tooltip/tooltip.tsx
+++ app/components/forms/tooltip/tooltip.tsx
@@ -18,9 +18,8 @@ export const Tooltip = ({ highlight = "?", text = test }: TooltipProps) => {
         onMouseEnter={() => setShow(true)}
         onMouseLeave={() => setShow(false)}
         data-tooltip-target="tooltip-top"
-        // data-tooltip-placement="top"
         type="button"
-        className="mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="border-2 rounded-md px-2"
       >
         {highlight}
       </button>
@@ -29,7 +28,7 @@ export const Tooltip = ({ highlight = "?", text = test }: TooltipProps) => {
         <div
           id="tooltip-top"
           role="tooltip"
-          className="absolute z-10 inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm tooltip dark:bg-gray-700"
+          className="absolute z-10 inline-block p-2 text font-medium text-black bg-white border rounded-md shadow-md tooltip"
         >
           {tooltip}
           <div className="tooltip-arrow" data-popper-arrow="Tooltip"></div>
--- app/rechner/zinsrechner-form/zinsrechner-form.tsx
+++ app/rechner/zinsrechner-form/zinsrechner-form.tsx
@@ -12,7 +12,7 @@ export const ZinsrechnerForm = () => {
   const [compoundInterest, setCompoundInterest] = useState<boolean>(true); // Новое состояние
   const [finalCapital, setFinalCapital] = useState<string>("");
   const buttonClass =
-    "bg-blue-500 hover:bg-blue-700 active:bg-blue-900 text-white font-bold py-1 px-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50";
+    "bg-teal-500 hover:bg-teal-700 active:bg-teal-900 focus:ring-teal-600 text-white font-bold py-1 px-2 rounded focus:outline-none focus:ring-2 focus:ring-opacity-50 min-w-48";
   const calculateAndSetFinalCapital = () => {
     const newFinalCapital = calculateEndkapital(
       initialCapital,
@@ -63,101 +63,131 @@ export const ZinsrechnerForm = () => {
     calculateAndSetFinalCapital();
   };
   return (
-    <div className="my-5 p-2 rounded border border-amber-200">
-      <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmit}>
-        <h4>ZinsrechnerForm</h4>
+    <div className="mx-auto mb-5 bg-opacity-5 p-5 rounded-lg shadow-xl text-2xl">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-start gap-5"
+      >
+        <h4 className="text-center">ZinsrechnerForm</h4>
         {/*  First line anfangskapital*/}
-        <fieldset className="flex gap-4 rounded border border-red-500 p-1 m-1">
-          <label htmlFor="anfangskapital" className="border-r border-gray-500">
-            Anfangskapital
-          </label>
-          <input
-            name="anfangskapital"
-            type="number"
-            defaultValue={initialCapital}
-            onChange={handleChange}
-            className="border-r border-gray-500"
-          />
-          <span className="border-r border-gray-500">€</span>
-          <span className="border-r border-gray-500">?</span>
+        <fieldset className="flex justify-start gap-5">
+          <div className="flex gap-0.5 min-w-max">
+            <label htmlFor="anfangskapital" className="inline-block">
+              Anfangskapital
+            </label>
+            <Tooltip highlight="?" text="Anfangskapital erklärung" />
+          </div>
+          <div className="flex gap-0.5">
+            <input
+              name="anfangskapital"
+              type="number"
+              defaultValue={initialCapital}
+              onChange={handleChange}
+              className="w-fit border rounded-md text-right"
+            />
+            <span className="font-semibold">€</span>
+          </div>
         </fieldset>
         {/*Zinssatz*/}
-        <fieldset className="flex gap-4 rounded border border-red-500 p-1 m-1">
-          <label htmlFor="zinssatz" className="border-r border-gray-500">
-            Zinssatz
-          </label>
-          <input
-            name="zinssatz"
-            type="number"
-            step="0.001"
-            defaultValue={interestRate}
-            onChange={handleChange}
-            className="border-r border-gray-500"
-          />
-          <span className="border-r border-gray-500">% p.a.</span>
-          <span className="border-r border-gray-500">?</span>
-        </fieldset>
+        <fieldset className="flex justify-start gap-5">
+          <div className="flex gap-0.5">
+            <label htmlFor="zinssatz" className="inline-block">
+              Zinssatz
+            </label>
+            <Tooltip highlight="?" text="Zinssatz erklärung" />
+          </div>
+          <div className="flex gap-0.5">
+            <input
+              name="zinssatz"
+              type="number"
+              step="0.001"
+              defaultValue={interestRate}
+              onChange={handleChange}
+              className="w-fit border rounded-md text-right"
+            />
+            <span className="font-semibold">% p.a.</span>
+          </div>
+        </fieldset>{" "}
         {/*Laufzeit*/}
-        <fieldset className="flex gap-4 rounded border border-red-500 p-1 m-1">
-          <label htmlFor="laufzeit" className="border-r border-gray-500">
-            Laufzeit
-          </label>
-          <input
-            name="laufzeit"
-            type="number"
-            defaultValue={duration}
-            onChange={handleChange}
-            className="border-r border-gray-500"
-          />
-          <span className="border-r border-gray-500">
-            <select id="laufzeit" name="laufzeitUnit" onChange={handleChange}>
+        <fieldset className="flex justify-start gap-5">
+          <div className="flex gap-0.5">
+            <label htmlFor="laufzeit" className="inline-block">
+              Laufzeit
+            </label>
+            <Tooltip highlight="?" text="Laufzeit erklärung" />
+          </div>
+          <div className="flex gap-0.5">
+            <input
+              name="laufzeit"
+              type="number"
+              defaultValue={duration}
+              onChange={handleChange}
+              className="w-fit border rounded-md text-right"
+            />
+            <select
+              id="laufzeit"
+              name="laufzeitUnit"
+              onChange={handleChange}
+              className="border rounded-md outline-none px-3 bg-white text-gray-800"
+            >
               <option value="jahre">Jahre</option>
               <option value="monate">Monate</option>
             </select>
-          </span>
-          <span className="border-r border-gray-500 font-bold">?</span>
+          </div>
         </fieldset>
         {/*Zinseszins*/}
-        <fieldset className="flex gap-4 rounded border border-red-500 p-1 m-1">
-          <label htmlFor="zinseszins" className="border-r border-gray-500">
-            Zinseszins
-          </label>
-          <div className="flex gap-2">
-            <input
-              type="radio"
-              id="ja"
-              name="janeinzinsansammlung"
-              value="Ja, Zinsansammlung"
-              onChange={handleChange}
-              checked={compoundInterest}
-            />
-            <label htmlFor="ja">Ja</label>
-            <input
-              type="radio"
-              id="nein"
-              name="janeinzinsansammlung"
-              value="Nein, Zinsauszahlung"
-              onChange={handleChange}
-              checked={!compoundInterest}
-            />
-            <label htmlFor="nein">Nein</label>
+        <fieldset className="flex justify-start gap-5">
+          <div className="flex gap-0.5">
+            <label htmlFor="zinseszins" className="inline-block">
+              Zinseszins
+            </label>
+            <Tooltip highlight="?" text="Zinseszins erklärung" />
           </div>
-          <span id="placeholder"></span>
-          <span className="border-r border-gray-500 font-bold">?</span>
-        </fieldset>
+          <div className="flex items-center gap-2">
+            <div className="flex items-center gap-1">
+              <input
+                type="radio"
+                id="ja"
+                name="janeinzinsansammlung"
+                value="Ja, Zinsansammlung"
+                onChange={handleChange}
+                checked={compoundInterest}
+                className="border rounded-sm text-right"
+              />
+              <label htmlFor="ja">Ja</label>
+            </div>
+            <div className="flex items-center gap-1">
+              <input
+                type="radio"
+                id="nein"
+                name="janeinzinsansammlung"
+                value="Nein, Zinsauszahlung"
+                onChange={handleChange}
+                checked={!compoundInterest}
+                className="border rounded-sm text-right"
+              />
+              <label htmlFor="nein">Nein</label>
+            </div>
+          </div>
+        </fieldset>{" "}
         {/*Endkapital*/}
-        <fieldset className="flex gap-4 rounded border border-red-500 p-1 m-1">
-          <label htmlFor="endkapital" className="border-r border-gray-500">
-            Endkapital
-          </label>
-          <input
-            name="endkapital"
-            defaultValue={finalCapital}
-            type="number"
-            className="border-r border-gray-500 font-bold text-2xl"
-          />
-          <span className="border-r border-gray-500">€</span>
-          <Tooltip highlight="?" text="endkapital erklärung" />
+        <fieldset className="flex justify-start gap-5">
+          <div className="flex gap-0.5">
+            <label htmlFor="endkapital" className="inline-block">
+              Endkapital
+            </label>
+            <Tooltip highlight="?" text="Endkapital erklärung" />
+          </div>
+          <div className="flex gap-0.5 items-center">
+            <input
+              disabled
+              name="endkapital"
+              defaultValue={finalCapital}
+              type="number"
+              className="w-fit border rounded-md text-right font-bold text-2xl"
+            />
+            <span>€</span>
+          </div>
         </fieldset>
         <fieldset className="flex gap-10">
           <button className={buttonClass} type="submit">
